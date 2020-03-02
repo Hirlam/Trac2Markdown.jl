fc06115ec9c1edb3fd50accd8f40fb45cee4b163
Roel Stappers <Roel.stappers@gmail.com>
2020-03-02 21:39:57 +0100

    Optionally download attachments

--- src/Trac2Markdown.jl
+++ src/Trac2Markdown.jl
@@ -10,44 +10,46 @@ const user = "roel"
 const passw = readline("$(ENV["HOME"])/.hirlampassw")
 
 
-const MARKDOWNDIR   = joinpath(dirname(pathof(Trac2Markdown)),"../docs/src/")
+const MARKDOWNDIR   = joinpath(dirname(pathof(Trac2Markdown)), "../docs/src/")
 const wikiurl       = "https://$user:$passw@hirlam.org/trac/wiki/HarmonieSystemDocumentation/"
 const attachmenturl = "https://$user:$passw@hirlam.org/trac/raw-attachment/wiki/HarmonieSystemDocumentation/"
 
 """
-    trac2markdown(relurl)
+    trac2markdown(relurl; getattachments)
 
-Writes markdown file to $MARKDOWNDIR, downloads attachment to $MARKDOWNDIR
-and returns names of linked wikipages
+Writes markdown file to $MARKDOWNDIR, and downloads attachment to $MARKDOWNDIR if getattachments=true.
+Returns names of linked wikipages
 """
-function trac2markdown(relurl::String)
+function trac2markdown(relurl::String; getattachments=false)
     @info "reading $relurl"
 
     # Retrieve page from wiki
     resp = HTTP.get("$wikiurl$relurl?format=txt")
     s = String(resp.body)
 
-    ## download attachments     
-    for attachment in eachmatch(r"\[raw-attachment:(.+?) .+?\]",s)
-         capture = attachment.captures[1]
-         @info "Downloading $capture"
-         mkpath(joinpath(MARKDOWNDIR,relurl))
-         download(joinpath(attachmenturl,relurl,capture),joinpath(MARKDOWNDIR,relurl,capture))
-    end
+    ## download attachments
+    if getattachments     
+        for attachment in eachmatch(r"\[raw-attachment:(.+?) .+?\]", s)
+            capture = attachment.captures[1]
+            @info "Downloading $capture"
+            mkpath(joinpath(MARKDOWNDIR, relurl))
+            download(joinpath(attachmenturl, relurl, capture), joinpath(MARKDOWNDIR, relurl, capture))
+        end
+    end 
 
     # Get names of linked subwiki's
     subwikis = String[]
-    for subwiki in eachmatch(r"\[wiki:(.+?) .+?\]",s)
+    for subwiki in eachmatch(r"\[wiki:(.+?) .+?\]", s)
         capture = subwiki.captures[1]
-        push!(subwikis,capture)
+        push!(subwikis, capture)
     end
 
     # convert text to markdown
     mdtxt  = trac2md(s)
 
     # write to file
-    mkpath(joinpath(MARKDOWNDIR,relurl))
-    write(joinpath(MARKDOWNDIR,relurl,"index.md"), mdtxt)
+    # mkpath(joinpath(MARKDOWNDIR, relurl))
+    write(joinpath(MARKDOWNDIR, "$relurl.md"), mdtxt)
 
     return subwikis
 end
@@ -68,8 +70,8 @@ function trac2md(s::String)
        r"== (.+?) =="                    => s"# \1",      # level 1 headers
        r"'''(.+?)'''"                    => s"**\1**",    # Bold 
        r"\[\[.+\]\]\r\n"                 => s"",          # Navigation symbols  (removed) 
-       #r"([^ ]*?HM_DATA[^ ]*)"       => s"`\1`",      # if word contains HM_DATA fomat it as code
-       #r"([^ ]*?hm_home/[^ ]*)"       => s"`\1`",      # if word contains cca/ fomat it as code
+       # r"([^ ]*?HM_DATA[^ ]*)"       => s"`\1`",      # if word contains HM_DATA fomat it as code
+       # r"([^ ]*?hm_home/[^ ]*)"       => s"`\1`",      # if word contains cca/ fomat it as code
        r"\[Back to the main page of the HARMONIE System Documentation\]\(HarmonieSystemDocumentation\)" => s"",
        r"\[\[Center\(end\)\]\]"         => s"",        # This one doesn't end in \r\n
     ]
