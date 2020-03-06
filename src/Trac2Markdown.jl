b3d4675f5069146bac735671a37ff2195bb275af
Roel Stappers <roel.stappers@met.no>
2020-03-06 13:23:59 +0100

    Update runtests.jl

--- src/Trac2Markdown.jl
+++ src/Trac2Markdown.jl
@@ -60,25 +60,28 @@ end
 function trac2md(s::String) 
 
     subs =  [ 
-       r"= '''Harmonie System Documentation''' =" => s"",
+       "= '''Harmonie System Documentation''' =" => "",
        r"\[(https?://.+?) (.+?)\]"        => s"[\2](\1)",  # hrefs http and https
        r"\[wiki:(.+?) (.+?)\]"           => s"[\2](\1)",  # Wiki links 
        r"\[source:(.+?) (.+?)\]"         => s"[\2](\1)",  # Source links
        r"\[raw-attachment:(.+?) (.+?)\]" => s"[\2](\1)",  # Attachements        
        r"\{\{\{([^\r\n]+?)\}\}\}"        => s"`\1`",      # Single line code blocks
-       r"\{\{\{"                         => s"```bash",   # Multiline code blocks (assumed to be bash)
-       r"\}\}\}"                         => s"```",       # multiline code blocks
+       "{{{"                             => "```bash",   # Multiline code blocks (assumed to be bash)
+       "}}}"                             => "```",       # multiline code blocks
+       r"==\s*'''\s*(.+?)\s*'''\s*=="    => s"## \1",     # level 2 header (not bold in md)
+       r"=\s*'''\s*(.+)\s*'''\s*="       => s"# \1",      # level 1 headers (not bold in md)
        r"===\s*(.+?)\s*==="              => s"### \1",    # level 3 headers
-       r"==\s*(.+?)\s*=="                    => s"## \1",     # level 2 headers
-       r"==\s*'''(.+?)'''\s*=="              => s"## \1",     # level 2 header (not bold in md)
-       r"=\s*'''(.+)'''\s*="                 => s"# \1",      # level 1 headers (not bold in md)
+       r"==\s*(.+?)\s*=="                => s"## \1",     # level 2 headers
+      
        r"=\s*(.+?)\s*="                  => s"# \1",      # level 1 headers 
-       r"'''(.+?)'''"                    => s"**\1**",    # Bold 
+       r"'''\s*(.+?)\s*'''"                    => s"**\1**",    # Bold 
+       "||"                              => "|",         # For tables 
        r"\[\[.+\]\]\r\n"                 => s"",          # Navigation symbols  (removed) 
        # r"([^ ]*?HM_DATA[^ ]*)"       => s"`\1`",      # if word contains HM_DATA fomat it as code
        # r"([^ ]*?hm_home/[^ ]*)"       => s"`\1`",      # if word contains cca/ fomat it as code
-       r"\[Back to the main page of the HARMONIE System Documentation\]\(HarmonieSystemDocumentation\)" => s"", 
-       r"\[\[Center\(end\)\]\]"         => s""        # This one doesn't end in \r\n
+       "[[Center(end)]]"                 => "" ,       # This one doesn't end in \r\n
+       "[Back to the main page of the HARMONIE System Documentation](HarmonieSystemDocumentation)" => ""
+       
     ]
 
     return foldl(replace, subs, init = s)
